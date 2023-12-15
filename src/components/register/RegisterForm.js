2991be338f34fdc82cdf90439295f7e6c8a6027d
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-12-15 09:51:38 -0500

    complete registration form

--- src/components/register/RegisterForm.js
+++ src/components/register/RegisterForm.js
@@ -22,77 +22,95 @@ const RegisterForm = () => {
   };
 
   return (
-    <div>
-      <img src={preprLogo} alt="prepr logo" />
+    <div className='registrationContainer'>
+      <div className='logoContainer'>
+        <img src={preprLogo} alt="prepr logo" />
+      </div>
+
 
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="firstName">First name:</label>
+          <label htmlFor="firstName" className="sr-only">First name:</label>
+          <i class="bi bi-person-rolodex"></i>
           <input
             type="text"
             id="firstName"
             name="firstName"
+            placeholder='First name'
             value={formData.firstName}
             onChange={handleInputChange}
             required
           />
         </div>
         <div>
-          <label htmlFor="lastName">Last name:</label>
+          <label htmlFor="lastName" className="sr-only">Last name:</label>
+          <i class="bi bi-person-rolodex"></i>
           <input
             type="text"
             id="lastName"
             name="lastName"
+            placeholder='Last name'
             value={formData.lastName}
             onChange={handleInputChange}
             required
           />
         </div>
         <div>
-          <label htmlFor="username">Username:</label>
+          <label htmlFor="username" className="sr-only">Username:</label>
+          <i class="bi bi-person"></i>
           <input
             type="text"
             id="username"
             name="username"
+            placeholder='Username'
             value={formData.username}
             onChange={handleInputChange}
             required
           />
         </div>
         <div>
-          <label htmlFor="password">Password:</label>
+          <label htmlFor="password" className="sr-only">Password:</label>
+          <i class="bi bi-lock-fill"></i>
           <input
             type="password"
             id="password"
             name="password"
+            placeholder="Password"
             value={formData.password}
             onChange={handleInputChange}
             required
           />
         </div>
         <div>
-          <label htmlFor="password">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <label htmlFor="confirmPassword" className="sr-only">Confirm Password:</label>
+          <i class="bi bi-check2-all"></i>
           <input
             type="password"
             id="confirmPassword"
             name="confirmPassword"
+            placeholder="Confirm password"
             value={formData.confirmPassword}
             onChange={handleInputChange}
             required
           />
         </div>
-        <button type="submit">Register</button>
+        <div>
+
+        </div>
+        <div>
+          <label htmlFor="password" className="sr-only">Email:</label>
+          <i class="bi bi-envelope"></i>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit" className='registerButton'>Register</button>
       </form>
     </div>
   );
