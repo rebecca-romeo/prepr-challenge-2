9e2be01ae4181f82c08ccf1b27c828fa66c52268
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-12-15 09:06:30 -0500

    registration form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import RegisterForm from './components/register/RegisterForm';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <RegisterForm />
 
       <Footer />
     </div>
---
+++ src/components/register/RegisterForm.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import preprLogo from "../../assets/preprLogo.png"
+
+const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+    email: '',
+    confirmPassword: ''
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <img src={preprLogo} alt="prepr logo" />
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
