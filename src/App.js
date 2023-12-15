cf766daf9b1662ad1230de83e633bf9066377cd4
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2023-12-15 10:33:50 -0500

    add Registration form component and Carousel component to Main component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import RegisterForm from './components/register/RegisterForm';
 import Carousel from './components/Carousel';
+import Main from './components/Main';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <RegisterForm />
-      <Carousel />
-
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import Carousel from "./Carousel";
+import RegisterForm from "./register/RegisterForm";
+
+const Main = () => {
+  return (
+    <main>
+      <Carousel />
+      <RegisterForm />
+    </main>
+  )
+}
+
+export default Main;
